ff7644b0d6fc20bb69df1d850aa7ddb3a961a0cb
mawalou14 <awaloum213@gmail.com>
2023-09-27 09:54:47 +0100

    added main page components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { MainComponent } from './main/main.component';
 import { DashboardComponent } from './main/pages/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'main', component: MainComponent, children: [
-    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: DashboardComponent }
-  ] }
+  {
+    path: 'main', component: MainComponent, children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent }
+    ]
+  }
 ];
 
 @NgModule({

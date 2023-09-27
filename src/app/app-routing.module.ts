f6f7bb82606e83c7f767e5a5924593d22bccc974
mawalou14 <awaloum213@gmail.com>
2023-09-27 10:57:00 +0100

    styled the sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { DashboardComponent } from './main/pages/dashboard/dashboard.component';
+import { WorkersComponent } from './main/pages/workers/workers.component';
+import { ResourcesComponent } from './main/pages/resources/resources.component';
 
 const routes: Routes = [
   {
     path: 'main', component: MainComponent, children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-      { path: 'dashboard', component: DashboardComponent }
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'workers', component: WorkersComponent },
+      { path: 'resources', component: ResourcesComponent }
     ]
   }
 ];

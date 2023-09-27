9525383f38b4781b54bc42bc38d81f379f34e85a
mawalou14 <awaloum213@gmail.com>
2023-09-27 10:40:41 +0100

    side nav toggle added

--- src/app/main/sidebar/sidebar.component.ts
+++ src/app/main/sidebar/sidebar.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+
+  @Output() sideNavToggled = new EventEmitter<boolean>();
+  @Input() sideNavStatus: boolean = true;
+  menuStatus: boolean;
+
+  activeLink: string = null;
+
+  constructor(
+    // private authService: AuthService
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.setActiveLink();
+      }
+    });
+  }
+
+  setActiveLink(): void {
+    const activeChild = this.activatedRoute.snapshot.firstChild;
+    if (activeChild) {
+      const { data } = activeChild.routeConfig;
+      if (data && data['activeLink']) {
+        this.activeLink = data['activeLink'];
+        return;
+      }
+    }
+    this.activeLink = null;
+  }
+
+  sideNavToggle() {
+    this.menuStatus = !this.menuStatus;
+    this.sideNavToggled.emit(this.menuStatus);
+  }
 
 }

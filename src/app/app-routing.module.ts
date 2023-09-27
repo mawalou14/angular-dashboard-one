95dae94bdcbe0ee4af993e836dd7b1d8afbd5f1e
mawalou14 <awaloum213@gmail.com>
2023-09-27 09:44:40 +0100

    added first rounting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { DashboardComponent } from './main/pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent, children: [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: DashboardComponent }
+  ] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
